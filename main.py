395ab72ad742b6d61c9491a6b4c1f72f0739d8da
Tr1cky3 <anshorullah3@gmail.com>
2025-11-20 08:31:55 +0700

    Add Assets

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Hello World!!!")
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Rogue × Birb")
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        
+        screen.fill((255, 127.5, 127.5))
+
+        pygame.display.flip()
+        clock.tick(60)
+
+pygame.quit()
